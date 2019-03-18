29a3cf0e1025b2580739b7b6e0fd21349875eeb9
Durbhakula, Venkata <venkata.durbhakula@hrblock.com>
2019-03-18 10:38:45 -0500

    Mocking http requests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import './index.css';
+import {getUsers} from './api/userApi';
 
-import numeral from 'numeral';
-
-const courseVal=numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseVal} for this awesome course`);
+// Populate table of users via API call.
+getUsers().then(result => {
+    let usersBody = "";
+  
+    result.forEach(user => {
+      usersBody+= `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+        </tr>`
+    });
+  
+    global.document.getElementById('users').innerHTML = usersBody;
+});
